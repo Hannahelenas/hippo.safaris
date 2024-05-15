e160ff88a7fece9a8336b1c4e18465f62eb4383f
Hannahelenas <hanna.eriksson@iths.se>
2024-05-15 12:38:46 +0200

    Installed mui and styled-components, tested a navbar template

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,36 @@
-import './App.css'
+import './App.css';
+
+// Testar importer
+
+import TextField from '@mui/material/TextField';
+import NavBar from './components/NavBar';
+
+
 
 function App() {
+    return (
+        <>
+            <NavBar />
+            <h1>Testar</h1>
+            <TextField
+                required
+                id="outlined-required"
+                label="Required"
+                defaultValue="Hello World"
+            />
+            <div>
+                {/* <IconButton aria-label="cart">
+                <StyledBadge badgeContent={1} color="secondary">
+                    <ShoppingCartIcon
+                        fontSize="medium"
+                        sx={{ color: 'red' }}
+                    />
+                </StyledBadge>
+                </IconButton> */}
 
-  return (
-    <>
-      
-        <h1>Testar</h1>
-
-    </>
-  )
+            </div>
+        </>
+    );
 }
 
-export default App
+export default App;
